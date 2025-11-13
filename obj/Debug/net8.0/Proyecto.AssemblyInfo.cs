675fc6e54125c02ef7ad5806f373d15380a7a393
VICTORIA JOANA KRIGER CONTI <49439065@est.ort.edu.ar>
2025-11-13 10:38:23 -0300

    avance

--- obj/Debug/net8.0/Proyecto.AssemblyInfo.cs
+++ obj/Debug/net8.0/Proyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b18ae3ec8339b691bb19faf4ea8cd79fd2ee1638")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
